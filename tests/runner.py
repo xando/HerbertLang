c58c31e546b9bf440892cb5595dfcfee554f4060
Sebastian Pawluś <sebastian.pawlus@gmail.com>
2023-08-27 00:08:12 +0200

    colors

--- tests/runner.py
+++ tests/runner.py
@@ -12,22 +12,20 @@ TEST_STDERR_FILE = "stderr"
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--binary", help="Interpreter Binary", required=True)
-    parser.add_argument("--tests", required=True)
+    parser.add_argument("--tests", help="Tests Directory", required=True)
 
     args = parser.parse_args()
 
-    for test_input_code_file in sorted(glob.glob(os.path.join(args.tests, "*", "in.h"))):
+    for test_input_code_file in sorted(glob.glob(os.path.join(args.tests, "*", TEST_CODE_FILE))):
 
-        test_name = os.path.dirname(test_input_code_file)
-        if "function_arguments_1" in test_name:
-            breakpoint()
+        test_name = os.path.basename(os.path.dirname(test_input_code_file))
 
         result = subprocess.run(
             [args.binary, test_input_code_file],
             capture_output=True,
             text=True
         )
-        
+
         if result.returncode == 0:
             expected_output = open(os.path.join(test_name, TEST_STDOUT_FILE)).read()
             current_output = result.stdout.strip()
@@ -35,7 +33,7 @@ def main():
             expected_output = open(os.path.join(test_name, TEST_STDERR_FILE)).read()
             current_output = result.stderr.strip()
 
-        print(f"Test:{test_name} ->", 'PASSED' if expected_output == current_output else 'FAILED')
-    
+        print(f"{test_name} ->", '\033[92mPASSED' if expected_output == current_output else '\033[91mFAILED', '\033[0m')
+
 if __name__ == "__main__":
     main()
