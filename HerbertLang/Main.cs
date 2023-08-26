975f8bf6a2ee49c0b22f5c49a95671681be7c5fd
Sebastian Pawluś <sebastian.pawlus@gmail.com>
2023-08-27 00:02:06 +0200

    CLI interface

--- HerbertLang/Main.cs
+++ HerbertLang/Main.cs
@@ -1,4 +1,25 @@
 
+using HerbertLang;
+
 public class Herbert {
-    static void Main() { }
+    static int Main(string[] args) {
+        if (args.Length == 0) {
+            Console.Error.WriteLine("Please provide a file path as an argument.");
+            return -1;
+        }
+
+        string filePath = args[0];
+
+        if (!File.Exists(filePath)) {
+            Console.Error.WriteLine($"The file '{filePath}' does not exist.");
+            return -1;
+        }
+
+        var inputCode = File.ReadAllText(filePath);
+        var outputCode = Interpreter.evalToString(inputCode);
+
+        Console.WriteLine(outputCode);
+
+        return 0;
+    }
 }
